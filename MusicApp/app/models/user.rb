cdd387686c0307f5d69166e505f18e0a64df7a2e
Anthony Chao <anthonyschaowder@gmail.com>
2022-07-06 17:01:13 -0400

    first commit - added user controller and views

---
+++ MusicApp/app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ApplicationRecord
+
+    after_initialize :ensure_session_token
+
+    validates :email, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+    validates :session_token, presence: true, uniqueness: true
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    attr_reader :password
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+end
